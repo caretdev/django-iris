04643fb95274d63ac73bae2a7ccc2507d55f2469
Dmitry Maslennikov <mrdaimor@gmail.com>
2022-03-19 21:17:52 +0400

    update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup
 
-setup()
+setup(dependency_links=[
+  'https://github.com/intersystems-community/iris-driver-distribution/raw/main/intersystems_irispython-3.2.0-py3-none-any.whl'
+])
